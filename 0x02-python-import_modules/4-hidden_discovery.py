14807af6cc3684f639ee4942897dc1c58e9bc57e
root <d20forluv@gmail.com>
2023-08-15 06:26:36 -0700

    wter

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    li = dir(hidden_4)
-    for i in range(0, len(li)):
-        if li[i][0] == '_' and li[i][1] == '_':
-            continue
-        else:
-            print('{}'.format(li[i]))
+    for i in dir(hidden_4):
+        if "__" not in i:
+            print(i)
