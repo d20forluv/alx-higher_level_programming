8744a031e742250da023447bfb708035116f649d
root <d20forluv@gmail.com>
2023-08-15 06:25:17 -0700

    2ert

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    l = len(argv)
+    import sys
     sum = 0
-    for i in range(1, l):
-        sum += int(argv[i])
-    print('{:d}'.format(sum))
+    for i in range(1, len(sys.argv)):
+        sum = sum + int(sys.argv[i])
+    print(sum)
