179116e98ac7624dbbd6c3bc6836ab9131deaf09
root <d20forluv@gmail.com>
2023-08-23 02:42:07 -0700

    11

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,21 +2,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - function with one arguments
- * @h: linked list pointer
+ * print_dlistint - Prints a doubly linked list in order
+ * @h: The head of the list
  *
- * Description: prints all the elements of doubly linked list
- * Return: number of nodes
+ * Return: The number of nodes in the doubly linked list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
+	const dlistint_t *node = h;
 
-	while (h)
+	for (; node; node = node->next, ++count)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
+		printf("%d\n", node->n);
 	}
+
 	return (count);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,7 +1,9 @@
-#ifndef LISTS_H_
-#define LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -9,13 +11,13 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * for Coding project
+ * for Holberton project
  */
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
+int n;
+struct dlistint_s *prev;
+struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
